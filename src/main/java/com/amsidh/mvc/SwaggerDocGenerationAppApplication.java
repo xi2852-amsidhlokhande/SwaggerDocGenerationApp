09494fec3562da139dd117457bdb439095241a76
Amsidh.Lokhande <amsidh.lokhande@xebia.com>
2023-01-03 19:29:48 +0530

    Able to generate classes using openapi-3 swagger file

--- src/main/java/com/amsidh/mvc/SwaggerDocGenerationAppApplication.java
+++ src/main/java/com/amsidh/mvc/SwaggerDocGenerationAppApplication.java
@@ -1,4 +1,4 @@
-package com.amsidh.mvc.SwaggerDocGenerationApp;
+package com.amsidh.mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
