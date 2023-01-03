822755f465ba4efcc584ad29a97f67b10c7b103f
Amsidh.Lokhande <amsidh.lokhande@xebia.com>
2023-01-03 17:15:22 +0530

    Initial commit

---
+++ src/main/java/com/amsidh/mvc/SwaggerDocGenerationApp/SwaggerDocGenerationAppApplication.java
@@ -0,0 +1,13 @@
+package com.amsidh.mvc.SwaggerDocGenerationApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerDocGenerationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerDocGenerationAppApplication.class, args);
+	}
+
+}
